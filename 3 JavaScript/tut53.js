5ca72ad211b125db5cd734d3e821125971059451
ZAIN Ul ABAIDEEN <mz415961@gmail.com>
2022-12-20 21:55:12 +0500

    Updloaded

---
+++ 3 JavaScript/tut53.js
@@ -0,0 +1,28 @@
+console.log('This is tut 53');
+let name ="Zain"
+let name1 ="umer"
+let name2="ali"
+let name3 ="alina"
+let greetText="Good Morning "
+console.log(name + "is a good boy");
+console.log(name1 + "is a good boy");
+console.log(name2 + "is a good boy");
+console.log(name3 + "is a good boy");
+function greet(name, greetText){
+    let name1='nAME1'
+    console.log(greetText + name );
+   
+    console.log(
+     name + "is a good boy");
+
+}
+greet(name, greetText);
+greet(name1, greetText);
+greet(name2, greetText);
+greet(name3, greetText);
+function sum(a,b,c){
+    d=a+b+c;
+    return d;
+}
+let ret=sum(3,5,5);
+console.log(ret);
